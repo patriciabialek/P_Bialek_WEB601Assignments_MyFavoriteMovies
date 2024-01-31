ffa4ba938e130f6bf5dfe728593e757fbd647d9b
Patricia <122388811+patriciabialek@users.noreply.github.com>
2024-01-31 03:55:56 +0000

    still have issues when running the app.

--- P_Bialek_MyFavoriteMovies/src/app/content-card/content-card.component.ts
+++ P_Bialek_MyFavoriteMovies/src/app/content-card/content-card.component.ts
@@ -13,11 +13,11 @@ import { Content } from '../helper-files/content-interface';
 export class ContentCardComponent implements OnInit{
   //allows data to be passed from the parent component to the child component 
   //Content-list passes down 'content' property(parent)
-  @Input() contentItems: Content;
+  @Input() content: Content;
 
   //Clicking on the image displays the Content's id and title in the console
-  showContentDetails() {
-    console.log(`Image ID: ${this.contentItems.id}, Image Title: ${this.contentItems.title}`);
+  showDetails() {
+    console.log(`Image ID: ${this.content.id}, Image Title: ${this.content.title}`);
   }
 
   constructor() {
