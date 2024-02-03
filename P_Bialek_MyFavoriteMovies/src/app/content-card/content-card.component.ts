68e1853b18aeaec1f147dc417918c7ac3c29dcb7
Patricia <122388811+patriciabialek@users.noreply.github.com>
2024-02-03 20:41:06 +0000

    Deleted old content-card component and created a new one, still the same error

--- P_Bialek_MyFavoriteMovies/src/app/content-card/content-card.component.ts
+++ P_Bialek_MyFavoriteMovies/src/app/content-card/content-card.component.ts
@@ -1,28 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ContentListComponent } from '../content-list/content-list.component';
+import { CommonModule } from '@angular/common';
 import { Content } from '../helper-files/content-interface';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-content-card',
   standalone: true,
-  imports: [CommonModule, ContentListComponent, RouterOutlet],
+  imports: [ContentListComponent, CommonModule],
   templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.scss'],
+  styleUrl: './content-card.component.scss'
 })
-
-export class ContentCardComponent{
-  //allows data to be passed from the parent component to the child component 
-  //Content-list passes down 'content' property(parent)
+export class ContentCardComponent {
   @Input() content: Content;
-
-  //Clicking on the image displays the Content's id and title in the console
+  
   showDetails() {
     console.log(`Image ID: ${this.content.id}, Image Title: ${this.content.title}`);
   }
-
-  constructor() {
-  
-  }
+ 
 }
