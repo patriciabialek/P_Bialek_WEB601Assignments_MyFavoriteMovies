3731d32291e6272c858ff5a865c2bffbe5eec29a
patriciabialek <w0738386@myscc.ca>
2024-02-03 20:28:09 -0500

    stackblitz

--- P_Bialek_MyFavoriteMovies/src/app/content-card/content-card.component.ts
+++ P_Bialek_MyFavoriteMovies/src/app/content-card/content-card.component.ts
@@ -1,47 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
+import { Component, Input } from '@angular/core';
+import { ContentListComponent } from '../content-list/content-list.component';
 import { CommonModule } from '@angular/common';
+import { Content } from '../helper-files/content-interface';
 
-//create an instance of your ContentList and add at least 3 valid items to it using the ContentList's add function in the Content-card's constructor.
 @Component({
   selector: 'app-content-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ContentListComponent, CommonModule],
   templateUrl: './content-card.component.html',
   styleUrl: './content-card.component.scss'
 })
-export class ContentCardComponent implements OnInit{
-  contentList:ContentList;
-
-  // Add at least 3 valid items to the contentList in the constructor
-  constructor() {
-    this.contentList = new ContentList;
-    this.contentList.addContentItem({
-      id: 1,
-      title: 'Coraline',
-      description: 'A young girl\'s discovery of a parallel world that initially appears ideal but turns sinister',
-      creator: 'Henry Selick',
-      type: 'Horror/Fantasy',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/en/3/36/Coraline_poster.jpg',
-    });
-    this.contentList.addContentItem({
-      id: 2,
-      title: 'Eternal Sunshine of the Spotless Mind',
-      description: 'A film that explores the concept of erasing memories to cope with heartbreak',
-      creator: 'Michel Gondry',
-      type: 'Romance/Sci-fi',
-      imgURL: 'https://m.media-amazon.com/images/I/71G7AybM3qL._AC_UF894,1000_QL80_.jpg',
-    });
-    this.contentList.addContentItem({
-      id: 3,
-      title: 'Shrek 2',
-      description: 'A humorous and adventurous journey to meet the in-laws after Shrek and Princess Fiona marry',
-      creator: 'Andrew Adamson',
-      type: 'Comedy/Fantasy',
-      imgURL: 'https://m.media-amazon.com/images/I/71PM3ZsLZaL._AC_UF1000,1000_QL80_.jpg',
-    });
+export class ContentCardComponent {
+  @Input() content: Content;
+  
+  showDetails() {
+    console.log(`Image ID: ${this.content.id}, Image Title: ${this.content.title}`);
   }
-  ngOnInit(){
-
-  }
+ 
 }
---
+++ P_Bialek_MyFavoriteMovies/src/app/content-list/content-list.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Content } from '../helper-files/content-interface';
+import {ContentCardComponent} from '../content-card/content-card.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-content-list',
+  standalone: true,
+  imports: [CommonModule, ContentCardComponent, RouterOutlet],
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
+})
+export class ContentListComponent {
+  contentArray: Content[] = [
+    {id: 1, 
+      title: "Coraline", 
+      description: "description...", 
+      creator: "Henry Selick", 
+      imgURL: "https://upload.wikimedia.org/wikipedia/en/3/36/Coraline_poster.jpg", 
+      type: "Fantasy", 
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      id: 2, 
+      title: "Shrek 2", 
+      description: "description...", 
+      creator: "Andrew Adamson", 
+      imgURL: "https://m.media-amazon.com/images/I/71PM3ZsLZaL._AC_UF1000,1000_QL80_.jpg", 
+      type: "Comedy", 
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      id: 3, 
+      title: "Eternal Sunshine of the Spotless Mind", 
+      description: "description...", 
+      creator: "Michel Gondry", 
+      imgURL: "https://m.media-amazon.com/images/I/71G7AybM3qL._AC_UF894,1000_QL80_.jpg", 
+      type: "Sci-Fi", 
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      id: 4, 
+      title: "Sinister", 
+      description: "description...", 
+      creator: "Scott Derrickson", 
+      imgURL: "https://m.media-amazon.com/images/I/81mfiTrZsFL._AC_UF1000,1000_QL80_.jpg", 
+      type: "Horror", 
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      id: 5, 
+      title: "Showgirls", 
+      description: "description...", 
+      creator: "Paul Verhoeven", 
+      imgURL: "https://m.media-amazon.com/images/M/MV5BNzcyMjExNjg3OF5BMl5BanBnXkFtZTgwOTY4Mjc1MDE@._V1_.jpg", 
+      type: "Thriller", 
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      id: 6, 
+      title: "Black Swan", 
+      description: "description...", 
+      creator: "Darren Aronofsky", 
+      imgURL: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8236892_v_v10_ar.jpg", 
+      type: "Thriller", 
+      tags: ["tag1", "tag2", "tag3"],
+    },
+  ];
+
+  constructor(){
+
+  }
+}
