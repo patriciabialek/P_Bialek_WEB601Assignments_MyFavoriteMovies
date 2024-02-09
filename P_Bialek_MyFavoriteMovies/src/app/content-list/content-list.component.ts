814aa315843d28ace1dffc13d8643e4124072015
Patricia <122388811+patriciabialek@users.noreply.github.com>
2024-02-09 19:28:15 +0000

    Created an input field and button that displays if a title exists or not.

--- P_Bialek_MyFavoriteMovies/src/app/content-list/content-list.component.ts
+++ P_Bialek_MyFavoriteMovies/src/app/content-list/content-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Content } from '../helper-files/content-interface';
 import {ContentCardComponent} from '../content-card/content-card.component';
 import {FilterTypePipe} from '../filter-type.pipe';
@@ -7,7 +8,7 @@ import {FilterTypePipe} from '../filter-type.pipe';
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [CommonModule, ContentCardComponent, FilterTypePipe],
+  imports: [CommonModule, ContentCardComponent, FilterTypePipe, FormsModule],
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
@@ -84,6 +85,19 @@ export class ContentListComponent {
       tags: ["Mobsters", "Hitmen", "Crime"],
     },
   ];
+
+  filterContent: Content[] = [];
+  title:string = '';
+  message: string = '';
+  isFound: boolean = false;
+
+  checkTitle(){
+    this.filterContent = this.contentArray.filter(item => item.title.toLowerCase() === this.title.toLowerCase());
+
+    this.isFound = this.filterContent.length > 0;
+    
+    this.message = this.isFound ? `Content with title '${this.title}' found.` : `Content with title '${this.title}' not found.`;
+  }
 
   constructor(){
 
