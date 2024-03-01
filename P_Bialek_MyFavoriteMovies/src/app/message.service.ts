0204bc655da6b012683542261c08d69096193351
Patricia <122388811+patriciabialek@users.noreply.github.com>
2024-02-29 21:58:35 -0500

    Update message.service.ts

--- P_Bialek_MyFavoriteMovies/src/app/message.service.ts
+++ P_Bialek_MyFavoriteMovies/src/app/message.service.ts
@@ -4,10 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
-  //initializing a new instance of 'Subject' (an array of strings[])
   messages: string[] = [];
 
-  //this method emits the each message subject ([message] array)
   add(message: string) {
     this.messages.push(message);
   }
