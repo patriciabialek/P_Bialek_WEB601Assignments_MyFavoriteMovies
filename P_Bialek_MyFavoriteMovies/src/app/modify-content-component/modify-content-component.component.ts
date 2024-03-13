d9ee2494bafb429de946cc2ad7ad34c9c5e7e938
Patricia <W0738386@myscc.ca>
2024-03-12 22:09:50 -0400

    created a new component

---
+++ P_Bialek_MyFavoriteMovies/src/app/modify-content-component/modify-content-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-content-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-content-component.component.html',
+  styleUrl: './modify-content-component.component.scss'
+})
+export class ModifyContentComponentComponent {
+
+}
